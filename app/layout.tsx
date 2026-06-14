a276ea04098b0c23ea2610d3b83e524cfda937a7
jashanj0tsingh <jshn.singh1@gmail.com>
2026-06-13 20:53:33 -0400

    seo related, adds sitemap and robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,20 @@ import "./globals.css";
 const firaCode = localFont({ src: "./FiraCode-VF.ttf" });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.jashanjotsingh.com"),
   title: "Jashan Singh | Software Engineer",
   description: "Personal portfolio of Jashan Singh",
+  openGraph: {
+    title: "Jashan Singh | Software Engineer",
+    description: "Personal portfolio of Jashan Singh",
+    url: "https://www.jashanjotsingh.com",
+    siteName: "Jashanjot Singh",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
